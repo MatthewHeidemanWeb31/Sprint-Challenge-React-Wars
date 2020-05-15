3f9102d57b481de30b373c8647e9d95eb9819f66
matthew-heideman <heidemanmr@yahoo.com>
2020-05-15 11:33:20 -0500

    Fetched Data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [ characters, setCharacters ] = useState([]);
+  const rmUrl = 'https://rickandmortyapi.com/api/';
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get(`${rmUrl}character`)
+      .then(response => {
+        console.log(response);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log('The data was not retrieved', error);
+      })
+  }, [])
 
   return (
     <div className="App">
