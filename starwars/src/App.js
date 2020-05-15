2fab39d36f501b40c82fce767b156d0f5d3c381a
matthew-heideman <heidemanmr@yahoo.com>
2020-05-15 12:54:24 -0500

    completed sprint with stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import {NavItem, Nav, NavLink, TabContent, TabPane} from 'reactstrap';
+import classnames from 'classnames';
+import styled from 'styled-components';
+import Character from './components/Character';
+import Dog from './components/Dog';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [ characters, setCharacters ] = useState([]);
+  const [ dogImgs, setDogImgs ] = useState([])
+  const [activeTab, setActiveTab] = useState('1');
   const rmUrl = 'https://rickandmortyapi.com/api/';
-
+  const dCEOUrl = 'https://dog.ceo/api/breed/hound/basset/images/random/30'
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+  const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background: white;
+    
+  `;
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -22,10 +39,49 @@ const App = () => {
         console.log('The data was not retrieved', error);
       })
   }, [])
+  useEffect(() => {
+    axios
+      .get(`${dCEOUrl}`)
+      .then(response => {
+        console.log(response);
+        setDogImgs(response.data.message)
+      })
+      .catch(error => {
+        console.log('The data was not retrieved', error);
+      })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Sprint Challenge</h1>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}>
+            Rick and Morty Characters
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}>
+              Doggos
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab} >
+        <TabPane tabId="1">
+          <Wrapper>
+            <Character characters = {characters} />
+          </Wrapper>
+        </TabPane>
+        <TabPane tabId='2'>
+          <Wrapper>
+            <Dog dogImgs = {dogImgs}/>
+          </Wrapper>
+        </TabPane>
+      </TabContent>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React from 'react';
+import { Col, Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText, CardImg} from 'reactstrap';
+import styled from 'styled-components';
+
+function Character(props){
+    const  characters  = props.characters;
+    console.log(characters);
+    const Wrapper = styled.div`
+        display: flex;
+        justify-content: center;
+    `;
+ return(
+     <Wrapper>
+        <Col sm = '11'>
+        {characters.map((item, i) => {
+        return  <Card key = {i} body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} key = {item.id} >
+                    <CardImg src={item.image} alt = {item.name} />
+                    <CardHeader>
+                        <CardTitle>
+                            Name: {item.name}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardSubtitle>
+                            Status: {item.status}
+                        </CardSubtitle>
+                        <CardText>
+                            Species: {item.species}
+                        </CardText> 
+                        <CardText>
+                        Known Homeworld and/or Universe: {item.origin.name}
+                        </CardText>
+                        <CardText>
+                            Episode Count: {item.episode.length}
+                        </CardText>
+                    </CardBody>
+                </Card>
+        })
+        }
+        </Col>
+    </Wrapper>
+    )
+}
+export default Character;
